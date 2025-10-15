74689dce45835b5ddfae1fd074d00b019aa39acc
Emin Apturaim <eminapturaim@gmail.com>
2025-10-15 11:00:48 +0300

    feat(router): instal and create react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const MarketPlacePage = lazy(() => import("./pages/MarketPlacePage"));
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      //errorElement: <ErrorPage>
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "/dashboard", element: <DashboardPage /> },
+        { path: "marketplace", element: <MarketPlacePage /> },
+      ],
+    },
+  ]);
+
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
 
