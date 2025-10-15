f0054a57d3625d113f63c1ef499f07b1236d0b20
Emin Apturaim <eminapturaim@gmail.com>
2025-10-15 17:08:43 +0300

    feat(Sidebar): create side navbar

---
+++ client/src/layouts/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./NavbarStyles.module.css";
+import SideNavbar from "./SideNavbar";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.navContainer}>
+          <NavLink to="/" className={styles.navLogo}>
+            <h3>TradinBIT</h3>
+          </NavLink>
+          <div className={styles.navSearch}>
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <input type="search" name="" placeholder="Search" />
+          </div>
+          <div className={styles.navProfileWrapper}>
+            <button className={styles.navThemeBtn}>
+              <i className="fa-solid fa-moon"></i>
+            </button>
+            <div className={styles.navProfile}>
+              <div className={styles.navProfilePhhoto}>
+                <img
+                  src="https://img.freepik.com/premium-vector/user-profile-icon-circle_1256048-12499.jpg?semt=ais_hybrid&w=740&q=80"
+                  alt="photo"
+                />
+              </div>
+              <h5>Name Here</h5>
+            </div>
+            <button className={styles.navMenuBtn}>
+              <i className="fa-solid fa-bars"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <SideNavbar />
+    </>
+  );
+}
---
+++ client/src/layouts/Navbar/SideNavbar.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "./NavbarStyles.module.css";
+
+export default function SideNavbar() {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <Link to={"/"}>
+          <i className="fa-solid fa-house"></i>
+          <h5>Home</h5>
+        </Link>
+        <Link to={"/marketoverview"}>
+          <i className="fa-solid fa-money-bill-trend-up"></i>
+          <h5>Market Overview</h5>
+        </Link>
+        <Link to={"/wallet"}>
+          <i className="fa-solid fa-wallet"></i>
+          <h5>My Wallet</h5>
+        </Link>
+        <Link to={"/wallet"}>
+          <i className="fa-solid fa-wallet"></i>
+          <h5>My Wallet</h5>
+        </Link>
+        <Link to={"/wallet"}>
+          <i className="fa-solid fa-wallet"></i>
+          <h5>My Wallet</h5>
+        </Link>
+      </div>
+    </>
+  );
+}
