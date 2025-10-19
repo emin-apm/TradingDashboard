384d10a9cb402b48e0dca153c9f9f87fcedb5667
Emin Apturaim <eminapturaim@gmail.com>
2025-10-19 15:28:04 +0300

    feat(DB): separate db connection

---
+++ server/src/config/connectDB.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async (mongoURI: string) => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("✅ Connected to MongoDB");
+  } catch (error: any) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
