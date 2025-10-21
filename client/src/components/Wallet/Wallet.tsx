1a5c18d913bbe2ebd86f8ea4db9f6daeae1d4701
Emin Apturaim <eminapturaim@gmail.com>
2025-10-21 14:29:45 +0300

    feat(Wallet): wallet loguc with histoy and balance added

--- client/src/components/Wallet/Wallet.tsx
+++ client/src/components/Wallet/Wallet.tsx
@@ -1,23 +1,37 @@
+import { useAuthStore } from "../../store/useAuthStore";
 import styles from "./WalletStyles.module.css";
+import { useNavigate } from "react-router-dom";
+
+type Trade = {
+  symbol: string;
+  side: "buy" | "sell";
+  price: number;
+  amount: number;
+  date: Date;
+};
 
 type WalletProps = {
   address: string | null;
-  balance: string | null;
+  balance: string | number | null;
   connectWallet: () => void;
   disconnectWallet: () => void;
+  tradeHistory: Trade[];
 };
-
 export default function Wallet({
   address,
   balance,
   connectWallet,
   disconnectWallet,
+  tradeHistory,
 }: WalletProps) {
+  const logout = useAuthStore((state) => state.logout);
+  const navigate = useNavigate();
+
   return (
     <section className={`container ${styles.wallet}`}>
       <div className={styles.walletBalance}>
         <h4>Total Balance:</h4>
-        <h1>{balance ? `${balance} ETH` : "$0"}</h1>
+        <h1>$ {balance}</h1>
         <h2>{address ?? "Wallet not connected"}</h2>
 
         <div className={styles.walletCta}>
@@ -35,40 +49,69 @@ export default function Wallet({
         </div>
       </div>
 
+      <div className={styles.walletCta}>
+        <button
+          className={styles.walletCtaBtn}
+          onClick={() => {
+            logout();
+            navigate("/");
+          }}
+        >
+          <i className="fa-solid fa-right-from-bracket"></i>
+          <p>Logout</p>
+        </button>
+      </div>
+
       <ul className={styles.walletTokens}>
-        <li className={styles.walletToken}>
-          <div>
-            <h4>Bitcoin</h4>
+        {tradeHistory.map((trade, index) => (
+          <li className={styles.walletToken} key={index}>
             <div>
-              <p>$55,000</p>
-              <div className={`small`}></div>
+              <h4>{trade.symbol}</h4>
+              <div>
+                <p>${trade.price}</p>
+                <div className={`small`}></div>
+              </div>
             </div>
-          </div>
-          <div className={styles.tokenRight}>
-            <h4>1 BTC</h4>
-            <p>$55,000</p>
-          </div>
-          <div className={styles.tradeButtons}>
-            <button
-              className={styles.buyBtn}
-              onClick={() => console.log("BTC Buy")}
-            >
-              Buy{" "}
-              <span>
-                <i className="fa-solid fa-arrow-up"></i>
-              </span>
-            </button>
-            <button
-              className={styles.sellBtn}
-              onClick={() => console.log("BTC Sell")}
-            >
-              Sell{" "}
-              <span>
-                <i className="fa-solid fa-arrow-down"></i>
-              </span>
-            </button>
-          </div>
-        </li>
+            <div className={styles.tokenRight}>
+              <h4>{trade.amount}</h4>
+              <p className={styles.date}>
+                {" "}
+                {new Date(trade.date).toLocaleDateString("en-US", {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </p>
+            </div>
+
+            <div className={styles.tradeButtons}>
+              {trade.side === "buy" && (
+                <button
+                  className={styles.buyBtn}
+                  onClick={() => console.log(`${trade.symbol} Buy`)}
+                >
+                  Buy{" "}
+                  <span>
+                    <i className="fa-solid fa-arrow-up"></i>
+                  </span>
+                </button>
+              )}
+
+              {/* Show only Sell button if side is 'sell' */}
+              {trade.side === "sell" && (
+                <button
+                  className={styles.sellBtn}
+                  onClick={() => console.log(`${trade.symbol} Sell`)}
+                >
+                  Sell{" "}
+                  <span>
+                    <i className="fa-solid fa-arrow-down"></i>
+                  </span>
+                </button>
+              )}
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   );
