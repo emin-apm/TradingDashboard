4b789e885c7542a2243fc21e1e666b4375961aed
Emin Apturaim <eminapturaim@gmail.com>
2025-10-22 16:43:04 +0300

    feat(router): add sell/buy router

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "../controller/authController";
+import tradeController from "../controller/tradeController";
 
 const router = Router();
 
 router.use("/user", authController);
+router.use("/order", tradeController);
 
 export default router;
