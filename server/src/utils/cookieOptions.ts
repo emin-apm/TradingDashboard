51be39e3fa9425abfbc0854376ebff217d4ea964
Emin Apturaim <eminapturaim@gmail.com>
2025-10-24 15:49:34 +0300

    fix(): cors

--- server/src/utils/cookieOptions.ts
+++ server/src/utils/cookieOptions.ts
@@ -3,6 +3,6 @@ import { CookieOptions } from "express";
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict" as "strict",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
