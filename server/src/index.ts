668d4c7df88b1b28462e3c69f05c0a479383621e
Emin Apturaim <eminapturaim@gmail.com>
2025-10-24 15:45:54 +0300

    fix()

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://trading-web331.web.app/"],
+    origin: ["http://localhost:5173", "https://trading-web331.web.app"],
     credentials: true,
   })
 );
